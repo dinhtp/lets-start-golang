f1b305483128f6c455f1589d872e8047b51f9c2e
dinhtp <dinhtp@smartosc.com>
2022-01-25 22:13:12 +0700

    Prepare Lesson Plan 6

--- 15.Functions/main.go
+++ 15.Functions/main.go
@@ -1,5 +1,23 @@
 package main
 
+/*
+@Resource
+- Go Tour:
+    + https://go.dev/tour/basics/4
+    + https://go.dev/tour/basics/5
+    + https://go.dev/tour/basics/6
+    + https://go.dev/tour/basics/7
+- Youtube:
+    + https://www.youtube.com/watch?v=CrgD_otSzDI
+- Go Documentation:
+    + https://go.dev/ref/spec#Function_types
+    + https://go.dev/ref/spec#Function_declarations
+    + https://go.dev/ref/spec#Function_literals
+    + https://go.dev/ref/spec#Built-in_functions
+- Go By Example:
+    + https://gobyexample.com/functions
+*/
+
 func main() {
 
 }
---
+++ 16.Advanced-Functions/main.go
@@ -0,0 +1,17 @@
+package main
+
+/*
+@Resource
+- Go Tour:
+    + https://go.dev/tour/moretypes/24
+    + https://go.dev/tour/moretypes/25
+    + https://go.dev/tour/moretypes/26
+- Youtube:
+    + https://www.youtube.com/watch?v=vdm04bVzkLg
+- Go Documentation:
+    + https://go.dev/ref/spec#Built-in_functions
+*/
+
+func main() {
+    // TODO: Go Tour Exercise - Fibonacci closure
+}
