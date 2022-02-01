d7a0bd74716c8987699454d0a27174045539d07a
dinhtp <dinhtp@smartosc.com>
2022-02-01 16:53:24 +0700

    Prepare Lesson Plan 8

--- 17.Mutable-Immutable/main.go
+++ 17.Mutable-Immutable/main.go
@@ -4,10 +4,8 @@ package main
 @Resource
 - Youtube:
     + https://www.youtube.com/watch?v=vtYTl4pNDSI
-- Go Documentation:
-    + https://go.dev/ref/spec#Built-in_functions
 */
 
 func main() {
-
+    // TODO: Declare a variable with mutable data type, change the variable without invoking directly the variable
 }
--- 19.Struct/main.go
+++ 19.Struct/main.go
@@ -1,5 +1,16 @@
 package main
 
+/*
+@Resource
+- Go Tour:
+    + https://go.dev/tour/moretypes/1
+    + https://go.dev/tour/moretypes/4
+- Youtube:
+    + https://www.youtube.com/watch?v=a4HcEsJ1hIE
+- Go By Example:
+    + https://gobyexample.com/pointers
+*/
+
 func main() {
 
 }
