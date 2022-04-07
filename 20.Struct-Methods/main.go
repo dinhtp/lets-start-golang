3253916f9765801a582457a1ff685408a6c9937c
dinhtp <dinhtp@smartosc.com>
2022-04-07 22:46:23 +0700

    Prepare Lesson Plan 10

--- 20.Struct-Methods/main.go
+++ 20.Struct-Methods/main.go
@@ -3,14 +3,33 @@ package main
 /*
 @Resource
 - Go Tour:
-    + https://go.dev/tour/moretypes/1
-    + https://go.dev/tour/moretypes/4
+    + https://go.dev/tour/methods/1
+    + https://go.dev/tour/methods/2
+    + https://go.dev/tour/methods/3
+    + https://go.dev/tour/methods/4
+    + https://go.dev/tour/methods/5
+    + https://go.dev/tour/methods/6
+    + https://go.dev/tour/methods/7
+    + https://go.dev/tour/methods/8
 - Youtube:
-    + https://www.youtube.com/watch?v=a4HcEsJ1hIE
+    + https://www.youtube.com/watch?v=5b8MMXgBnp0
+- Go Documentation:
+    + https://go.dev/ref/spec#Method_expressions
+    + https://go.dev/ref/spec#Method_values
 - Go By Example:
-    + https://gobyexample.com/pointers
+    + https://gobyexample.com/methods
 */
 
 func main() {
+    // TODO: Declare a struct named "Rectangle" which contains 2 properties: length, width (float64)
+    // Declare 2 struct methods named "area" and "perimeter" that calculate the area and the perimeter of the rectangle
+    // Implement the calculation logic and print out the result
 
+    // TODO: Declare a struct named "TimeParser" which contains 1 property: stringTime (string)
+    // "stringTime" property will take the time.RFC3339 input (sample input: "2006-01-02T15:04:05Z07:00")
+    // Declare 4 struct methods named "format1", "format2", "format3", "format4".
+    // "format1" method will parse the "stringTime" property and print out the time in format "yyyy-mmm-dd hh:mm:s"
+    // "format2" method will parse the "stringTime" property and print out the time in format "dd-mmm-yyyy hh:mm:s"
+    // "format3" method will parse the "stringTime" property and print out the time in format "hh:mm:s"
+    // "format4" method will parse the "stringTime" property and print out the time in format "dd-mmm-yyyy"
 }
--- 21.Interface/main.go
+++ 21.Interface/main.go
@@ -3,14 +3,28 @@ package main
 /*
 @Resource
 - Go Tour:
-    + https://go.dev/tour/moretypes/1
-    + https://go.dev/tour/moretypes/4
+    + https://go.dev/tour/methods/9
+    + https://go.dev/tour/methods/10
+    + https://go.dev/tour/methods/11
+    + https://go.dev/tour/methods/12
+    + https://go.dev/tour/methods/13
+    + https://go.dev/tour/methods/14
+    + https://go.dev/tour/methods/15
+    + https://go.dev/tour/methods/16
+    + https://go.dev/tour/methods/17
+    + https://go.dev/tour/methods/18
 - Youtube:
-    + https://www.youtube.com/watch?v=a4HcEsJ1hIE
+    + https://www.youtube.com/watch?v=lh_Uv2imp14
+- Go Documentation:
+    + https://go.dev/ref/spec#Interface_types
 - Go By Example:
-    + https://gobyexample.com/pointers
+    + https://gobyexample.com/interfaces
 */
 
 func main() {
+    // TODO: Go Tour Exercise - Stringers
 
+    // TODO: Declare an interface named "object", which contains a method named "volume".
+    // The "volume" method will return a float64 value which is the volume of the object.
+    // Declare a number of 3D objects (eg: cube, sphere, etc.) that implement the "object" interface.
 }
